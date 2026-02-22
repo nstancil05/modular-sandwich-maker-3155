9bd4ca8a0bac5336867e63fb58cfb02a3b6c842b
noahs <nstancil@charlotte.edu>
2026-02-22 18:05:13 -0500

    Implement Cashier class and complete main program loop
    - Filled in process_coins() to accept coin input
    - Filled in transaction_result() to validate payment and give change
    - Completed main() with ordering loop and correct modules

--- cashier.py
+++ cashier.py
@@ -3,11 +3,21 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        total = int(input("how many large dollars?: ")) * 1.00
+        total += int(input("how many half dollars?: ")) * 0.50
+        total += int(input("how many quarters?: ")) * 0.25
+        total += int(input("how many nickels?: ")) * 0.05
+        return total
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        """Return True when the payment is accepted, or False if money is insufficient."""
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            if change > 0:
+                print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
--- main.py
+++ main.py
@@ -13,7 +13,19 @@ cashier_instance = Cashier()
 
 
 def main():
-    ###  write the rest of the codes ###
+    is_on = True
+    while is_on:
+        choice = input("What would you like? (small/ medium/ large/ off): ").lower()
+        if choice == "off":
+            is_on = False
+        elif choice in recipes:
+            sandwich = recipes[choice]
+            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, sandwich["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
+        else:
+            print("Sorry that is not a valid option.")
 
 if __name__=="__main__":
     main()
